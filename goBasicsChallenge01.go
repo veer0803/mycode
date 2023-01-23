3c69d012a237ad06c9391647bc84bd4f4157e950
veer0803 <veeramaddipatla@gmail.com>
2023-01-23 16:49:39 +0000

    learning about go basics

---
+++ goBasicsChallenge01.go
@@ -0,0 +1,18 @@
+
+/* Alta3 Research | RZFeeser
+   Variables - Package and function level   */
+
+package main
+
+import "fmt"
+
+
+func main() {
+
+        var name, city,state  = "veer", "eden" ,  "mn"       
+
+        fmt.Println("My name is :", name)
+           fmt.Println("My city is :", city)
+		      fmt.Println("My state is :", state)
+}
+
